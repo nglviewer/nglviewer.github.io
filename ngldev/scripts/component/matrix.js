c6c5928b586dd886a10880ed37f9d643cb9545b1
Alexander Rose <alex.rose@rcsb.org>
2017-05-12 17:10:14 -0700

    ngldev update

---
+++ ngldev/scripts/component/matrix.js
@@ -0,0 +1,13 @@
+
+stage.loadFile( "data://1crn.pdb" ).then( function( o ){
+    o.addRepresentation( "cartoon" );
+    stage.autoView();
+} );
+
+stage.loadFile( "data://1crn.pdb" ).then( function( o ){
+    o.setPosition( [ 20, 0, 0 ] );
+    o.setRotation( [ 2, 0, 0 ] );
+    o.setScale( 0.5 );
+    o.addRepresentation( "cartoon", { color: "orange" } );
+    stage.autoView();
+} );
