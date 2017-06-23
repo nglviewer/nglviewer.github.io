8465a8392e3ea2b6d4d35f2b9ceea69361e96b70
Alexander Rose <alex.rose@rcsb.org>
2017-06-22 17:25:47 -0700

    ngldev update

--- ngldev/js/gui.js
+++ ngldev/js/gui.js
@@ -1484,6 +1484,7 @@ NGL.StructureComponentWidget = function( component, stage ){
             } );
         }
         var queue = new NGL.Queue( fn, e.target.files );
+        e.target.value = "";
     }
 
     var framesInput = document.createElement( "input" );
@@ -2186,14 +2187,18 @@ NGL.TrajectoryComponentWidget = function( component, stage ){
                 .setMarginRight( "69px" )
         );
 
+    function setFrame( value ){
+        frame.setValue( value );
+        frameRange.setValue( value );
+        numframes.clear().add( frame.setWidth( "70px" ) );
+    }
+
     function init( value ){
 
-        numframes.clear().add( frame.setWidth( "70px" ) );
         frame.setRange( -1, value - 1 );
         frameRange.setRange( -1, value - 1 );
 
-        frame.setValue( traj.currentFrame );
-        frameRange.setValue( traj.currentFrame );
+        setFrame( traj.currentFrame );
 
         if( component.defaultStep !== undefined ){
             step.setValue( component.defaultStep );
@@ -2208,12 +2213,7 @@ NGL.TrajectoryComponentWidget = function( component, stage ){
     }
 
     signals.gotNumframes.add( init );
-
-    signals.frameChanged.add( function( value ){
-        frame.setValue( value );
-        frameRange.setValue( value );
-        numframes.clear().add( frame.setWidth( "70px" ) );
-    } );
+    signals.frameChanged.add( setFrame );
 
     // Name
 
