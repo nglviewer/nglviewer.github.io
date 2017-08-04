231bdef843c4e9d30e8652ea8633b1d98d0e1859
Alexander Rose <alexander.rose@weirdbyte.de>
2017-08-04 16:04:01 +0200

    cleanup

--- scripts/color/resname.js
+++
@@ -1,6 +0,0 @@
-
-stage.loadFile('data://1blu.mmtf').then(function (o) {
-  o.addRepresentation('cartoon', { color: 'resname' })
-  o.addRepresentation('ball+stick', { color: 'resname' })
-  o.autoView()
-})
